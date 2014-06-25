0e62eaeb077cc5696a9c22025da2a0af78959596
BOSS <BOSS@Murilo-PC>
2014-06-25 19:50:00 -0300

    ATUALIZACAO CLASSES

--- AnaliseLexica/src/gals/Constants.java
+++ AnaliseLexica/src/gals/Constants.java
@@ -1,14 +1,14 @@
 package gals;
 
-public interface Constants extends ScannerConstants
+public interface Constants extends ScannerConstants, ParserConstants
 {
     int EPSILON  = 0;
     int DOLLAR   = 1;
 
-    int t_ID = 2;
-    int t_Literal = 3;
-    int t_Inteiro = 4;
-    int t_Real = 5;
+    int t_id = 2;
+    int t_literal = 3;
+    int t_num_int = 4;
+    int t_num_real = 5;
     int t_TOKEN_6 = 6; //";"
     int t_TOKEN_7 = 7; //","
     int t_TOKEN_8 = 8; //"."
@@ -28,30 +28,38 @@ public interface Constants extends ScannerConstants
     int t_TOKEN_22 = 22; //".."
     int t_TOKEN_23 = 23; //"<>"
     int t_TOKEN_24 = 24; //"<="
-    int t_programa = 25;
-    int t_var = 26;
-    int t_caracter = 27;
-    int t_cadeia = 28;
-    int t_procedimento = 29;
-    int t_inicio = 30;
-    int t_fim = 31;
-    int t_inteiro = 32;
-    int t_booleano = 33;
-    int t_funcao = 34;
-    int t_se = 35;
-    int t_entao = 36;
-    int t_senao = 37;
-    int t_leia = 38;
-    int t_escreva = 39;
-    int t_ou = 40;
-    int t_e = 41;
-    int t_nao = 42;
-    int t_falso = 43;
-    int t_verdadeiro = 44;
-    int t_de = 45;
-    int t_faca = 46;
-    int t_real = 47;
-    int t_vetor = 48;
-    int t_enquanto = 49;
+    int t_TOKEN_25 = 25; //">="
+    int t_TOKEN_26 = 26; //"{"
+    int t_TOKEN_27 = 27; //"}"
+    int t_programa = 28;
+    int t_var = 29;
+    int t_caracter = 30;
+    int t_cadeia = 31;
+    int t_procedimento = 32;
+    int t_inicio = 33;
+    int t_fim = 34;
+    int t_inteiro = 35;
+    int t_booleano = 36;
+    int t_funcao = 37;
+    int t_se = 38;
+    int t_entao = 39;
+    int t_senao = 40;
+    int t_leia = 41;
+    int t_escreva = 42;
+    int t_ou = 43;
+    int t_e = 44;
+    int t_nao = 45;
+    int t_falso = 46;
+    int t_verdadeiro = 47;
+    int t_de = 48;
+    int t_faca = 49;
+    int t_real = 50;
+    int t_vetor = 51;
+    int t_enquanto = 52;
+    int t_metodo = 53;
+    int t_ref = 54;
+    int t_val = 55;
+    int t_retorne = 56;
+    int t_div = 57;
 
 }
